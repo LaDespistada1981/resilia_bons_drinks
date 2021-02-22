f55f19c8a784be709014e204452cf3d0d8da411f
Roberta Santos <robertasantosrj@gmail.com>
2021-02-22 00:32:04 -0300

    *header component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,34 +1,33 @@
+import '../css/Form.css'
 import React, { Component } from 'react'
 
 export default class Form extends Component {
     render() {
         return (
             <div className="Form-contato content">
-                <h1 className="title contato-title">Contato</h1>
-                <img src="../src/img/contact-pic" alt=""/>
+                <h1 className="title-contato">Contato</h1>
+                <img className="contact-pic" src="../img/contact-pic.jpg" alt=""/>
                 <div className="form">
                     <form className="contact-form">
                         <div className="item-form">
-                            <label htmlFor="name">Nome</label>
+                            <label htmlFor="name">Nome: </label>
                             <input type="text" name="name" id=""/>
                         </div>
                         <div className="item-form">
-                            <label htmlFor="email">E-mail</label>
+                            <label htmlFor="email">E-mail: </label>
                             <input type="email" name="email" id=""/>
                         </div>
                         <div className="item-form">
-                            <label htmlFor="message">Mensagem</label>
+                            <label htmlFor="message">Mensagem: </label>
                             <textarea name="message" id="" cols="30" rows="10"></textarea>
                         </div>
                         <div className="item-form">
-                            <button type="submit">Enviar</button>
+                        <button className="btn-contact highlight-secondary " type="submit">Enviar</button>
                         </div>
-
-                        
-                        
                     </form>
                 </div>
             </div>
-        )
+            )
+        }
     }
-}
+    
