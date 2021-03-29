99efb38acf79b9b275ad3e3e913d33f3c25dda88
Roberta Santos <robertasantosrj@gmail.com>
2021-03-29 00:10:12 -0300

    Drinks

--- src/pages/Time/Time.jsx
+++ src/pages/Time/Time.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styles from "../Time/Time.module.css";
 import Card from "../../components/Card/Card";
-import TextBox from "../../components/TextBox/TextBox";
 import imgJuliette from "../../img/juliette_bbb.jpg";
 import imgRobertinha from "../../img/robertinha.jpg";
 import imgDayane from "../../img/dayane_bbb.jpg";
@@ -15,33 +14,9 @@ const Time = (props) => {
         <div className={styles.linha1}>
           <div className={styles.card}>
             <Card nome="Roberta Santos" imagem={imgRobertinha} />
-            <TextBox
-              className={styles.textBox}
-              content="Roberta é carioca e entre seus hobbies "
-            />
-          </div>
-          <div className={styles.card}>
             <Card nome="Dayane Mello" imagem={imgDayane} />
-            <TextBox
-              className={styles.textBox}
-              content="Dayane Mello é a modelo brasileira protagonista do BBB italiano. "
-            />
-          </div>
-        </div>
-        <div className={styles.linha2}>
-          <div className={styles.card}>
             <Card nome="Juliette Freire" imagem={imgJuliette} />
-            <TextBox
-              className={styles.textBox}
-              content="Juliette é paraibana e favorita ao BBB21. "
-            />
-          </div>
-          <div className={styles.card}>
             <Card nome="Camila de Lucas" imagem={imgCamila} />
-            <TextBox
-              className={styles.textBox}
-              content="Camila é a participante de Longe Iguaçú. "
-            />
           </div>
         </div>
       </section>
